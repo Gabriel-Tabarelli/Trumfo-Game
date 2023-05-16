e0d843ebc7e25909a338df70cbfd106260834ab3
gabriel_tabarelli <gabriel_tabarelli@estudante.sc.senai.br>
2023-05-16 18:39:34 -0300

    WebSocket_1.2

--- src/main/java/net/weg/projeto/webSocket/RequisicaoBatalhaController.java
+++ src/main/java/net/weg/projeto/webSocket/RequisicaoBatalhaController.java
@@ -1,15 +1,15 @@
 package net.weg.projeto.webSocket;
 
-import net.weg.projeto.model.entity.Jogador;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.util.HtmlUtils;
 
 public class RequisicaoBatalhaController {
 
-    @MessageMapping("/jogador/{id}/requisicao")
+    @MessageMapping("/jogador/{id}") // Não funciona!
     @SendTo("/topic/jogador/{id}")
-    public String requisicaoBatalha(Jogador jogador, @DestinationVariable String id) {
-        return new RequisicaoBatalha(jogador).requesicao();
+    public String requisicaoBatalha(String mensagem, @DestinationVariable String id) {
+        return HtmlUtils.htmlEscape(mensagem);
     }
 }
